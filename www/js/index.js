e3a36781a99fb07786525265094739636b2bc02a
James Messinger <james.messinger@gmail.com>
2015-03-10 17:23:21 -0500

    Placeholder Cordova project

---
+++ www/js/index.js
@@ -0,0 +1,37 @@
+(function() {
+    'use strict';
+
+    document.addEventListener('deviceready', onDeviceReady, false);
+
+    function onDeviceReady() {
+        if (typeof(device) !== 'undefined') {
+            // Display device information
+            document.getElementById('device-cordova').innerText = device.cordova;
+            document.getElementById('device-model').innerText = device.model;
+            document.getElementById('device-platform').innerText = device.platform;
+            document.getElementById('device-uuid').innerText = device.uuid;
+            document.getElementById('device-version').innerText = device.version;
+
+            if (device.platform === 'iOS') {
+                document.getElementById('browser-name').innerText = 'Mobile Safari';
+            }
+            else if (device.platform === 'Android') {
+                document.getElementById('browser-name').innerText = 'Chrome Mobile';
+            }
+            else if (device.platform.indexOf('Win') === 0) {
+                document.getElementById('browser-name').innerText = 'IE Mobile';
+            }
+        }
+
+        // Display the user agent string
+        document.getElementById('user-agent').innerText = navigator.userAgent;
+
+        // Wire-up the form
+        var theForm = document.getElementById('theForm');
+        var theUrl = document.getElementById('theUrl');
+
+        theUrl.onchange = theUrl.onkeydown = theUrl.onkeyup = function() {
+            theForm.action = theUrl.value;
+        };
+    }
+})();
